216c2fbaf85f374186b1fa552ea560bc9a6b8f67
ikeroed04060 <ikeroed04060@gmail.com>
2024-04-24 09:48:35 -0500

    quizz del tercer corte

---
+++ quizz_3/src/components/OtroGeneric.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <ion-item>
+      <ion-label>{{ label }}</ion-label>
+      <ion-input v-bind="$attrs" :type="type" :id="id" :value="value" :placeholder="placeholder" :clearInput="clearInput" :debounce="debounce" :disabled="disabled" :maxlength="maxlength" :min="min" :max="max" :step="step"></ion-input>
+     </ion-item>
+  </template>
+  
+  <script lang="ts">
+  import { IonInput, IonLabel } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    components: { IonLabel, IonInput },
+    props: {
+        label: {
+            type: String,
+            required: false
+         },
+   
+      name: String,
+      expand: String,
+      color: String,
+      size: String,
+      fill: String,
+      shape: String,
+      disabled: Boolean,
+      type: String,
+      href: String
+    }
+  });
+  </script>
+  
+  <style scoped>
+    /* Estilo global */
+  </style>
+  
+  
