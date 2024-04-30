2268be49f84e4249a9dd6b30c8fc57d06ddb1d05
Ikeroed04060 <132359845+Ikeroed04060@users.noreply.github.com>
2024-04-30 06:43:35 -0500

    Add files via upload

---
+++ quizz2_3/src/components/PersonaComponent.vue
@@ -0,0 +1,13 @@
+<template>
+<InputComponent id="id" name="id" label="ID: " />
+<InputComponent id="primerNombre" name="primerNombre" label="Primer Nombre " />
+<InputComponent id="segundoNombre" name="segundoNombre" label="Segundo Nombre: " />
+<InputComponent id="primerApellido" name="primerApellido" label="Primer Apellido: " />
+<InputComponent id="segundoApellido" name="segundoApellido" label="Segundo Apellido: " />
+<InputComponent id="documento" name="documento" label="Tipo de documento: " />
+<InputComponent id="numeroDocumento" name="numeroDocumento" label="Número de Documento: " />
+</template>
+
+<script setup lang="ts">
+import InputComponent from './InputComponent.vue';
+</script>
---
+++ quizz2_3/src/views/ClientePage.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+      <ion-header :translucent="true">
+        <ion-toolbar>
+          <ion-title>Carrito de compra</ion-title>
+        </ion-toolbar>
+      </ion-header>  
+      <ion-content>       
+        <div id="login-container">  
+          <div id="login-form">
+            <PersonaComponent/>
+            <InputComponent id="direccion" name="direccion" label="Dirección: " />
+            <InputComponent id="metodoPago" name="metodoPago" label="Método Pago: " />            
+          </div>
+  
+          <div> 
+             <!--Traer el componente de los botones de la crud  -->
+            <CrudButtonComponent />
+          </div>          
+        </div>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script setup lang="ts">
+  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+  import InputComponent from '@/components/InputComponent.vue';
+  import CrudButtonComponent from '@/components/CrudButtonComponent.vue';
+import PersonaComponent from '@/components/PersonaComponent.vue';
+  </script>
+  
+  <style scoped src="../theme/container.css"></style>
+
